f8de892da651e9aa6c1621f56d5dad2aae0706b4
Olalekan Lasabi <lekanlasabi@gmail.com>
2023-04-15 09:56:00 -0400

    esemie assessment

---
+++ hello-world/app.ts
@@ -0,0 +1,32 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ *
+ */
+
+export const lambdaHandler = async (event: APIGatewayProxyEvent, context:Context): Promise<APIGatewayProxyResult> => {
+    try {
+        const name = event.queryStringParameters?.name
+        const message = `Hello, ${name || 'Lasabi'}!`
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message
+            }),
+        };
+    } catch (err) {
+        console.log(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'some error happened',
+            }),
+        };
+    }
+};
